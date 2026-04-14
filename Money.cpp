00ccf3f3d43c26b7b3fbd480a61b489237838e41
Комова Надежда ТКИ-141 <komovanadenka@yandex.ru>
2026-04-14 14:47:07 +0300

    Update Money.cpp

--- Money.cpp
+++ Money.cpp
@@ -2,9 +2,8 @@
 #include <iostream>
 #include <cmath>
 #include <cstdlib>
-#include <utility>
-#include <limits>
-
+#include <utility> 
+#include <limits>  
 Money::Money(const int rub, const double kop)
 {
     double total = rub * 100.0 + kop;
@@ -12,26 +11,23 @@ Money::Money(const int rub, const double kop)
 }
 Money::Money(const double kop) : kopeks(kop) {}
 Money::Money(const Fractional f) : kopeks(f) {}
-
-Money::Money(const Money &other)
+Money::Money(const Money& other)
 {
     this->kopeks = other.kopeks;
 }
-Money::Money(Money &&other)
+Money::Money(Money&& other)
 {
-    std::swap(*this, other);
+    std::swap(*this, other); 
 }
-Money &Money::operator=(const Money &other)
+Money& Money::operator=(const Money& other)
 {
-    if (this == &other)
-        return *this;
+    if (this == &other) return *this;
     this->kopeks = other.kopeks;
     return *this;
 }
-Money &Money::operator=(Money &&other)
+Money& Money::operator=(Money&& other)
 {
-    if (this == &other)
-        return *this;
+    if (this == &other) return *this;
     std::swap(*this, other);
     return *this;
 }
@@ -47,7 +43,6 @@ Money Money::operator-(const Money other) const
 }
 double Money::operator/(const Money other) const
 {
-    // Заодно исправили и здесь, чтобы препод не придралась!
     if (std::abs(other.toKopeks()) < std::numeric_limits<double>::epsilon())
     {
         std::cout << "Ошибка, деление на ноль\n";
@@ -56,12 +51,12 @@ double Money::operator/(const Money other) const
     Fractional result = this->kopeks / other.kopeks;
     return result.toDouble();
 }
-Money Money::multiply(const double val) const
+Money Money::operator*(const double val) const
 {
     Fractional result = this->kopeks * val;
     return Money(result);
 }
-Money Money::divide(const double val) const
+Money Money::operator/(const double val) const
 {
     if (std::abs(val) < std::numeric_limits<double>::epsilon())
     {
@@ -70,6 +65,10 @@ Money Money::divide(const double val) const
     }
     Fractional result = this->kopeks / val;
     return Money(result);
+}
+Money operator*(const double val, const Money m)
+{
+    return m * val;
 }
 double Money::toKopeks() const
 {
@@ -90,9 +89,8 @@ std::ostream &operator<<(std::ostream &os, const Money m)
 }
 Money Money::read()
 {
-    int rub = 0;
+    int rub = 0; 
     double kop = 0.0;
-
     std::cout << "Введите рубли и копейки через пробел: ";
     std::cin >> rub >> kop;
     if (std::cin.fail())
