6b829aa0df38747c8dfb85f70dca02013d293355
Комова Надежда ТКИ-141 <komovanadenka@yandex.ru>
2026-05-20 10:19:18 +0300

    Update DoublyLinkedList.cpp

--- DoublyLinkedList.cpp
+++ DoublyLinkedList.cpp
@@ -2,160 +2,152 @@
 #include <sstream>
 #include <stdexcept>
 
-/* ================= Node ================= */
-
-DoublyLinkedList::Node::Node(int value)
-    : data(value), prev(nullptr), next(nullptr)
+DoublyLinkedList::Node::Node(const int value)
+: data(value), prev(nullptr), next(nullptr)
 {
 }
-
-/* ================= Constructors ================= */
 
 DoublyLinkedList::DoublyLinkedList()
-    : head(nullptr), tail(nullptr), size(0)
+: head(nullptr), tail(nullptr), size(0)
 {
 }
 
-DoublyLinkedList::DoublyLinkedList(std::initializer_list<int> list)
-    : head(nullptr), tail(nullptr), size(0)
+DoublyLinkedList::DoublyLinkedList(const std::initializer_list<int> list)
+: head(nullptr), tail(nullptr), size(0)
 {
-    for (int value : list)
-        push_back(value);
+for (int value : list)
+push_back(value);
 }
-
-/* ================= Destructor ================= */
 
 DoublyLinkedList::~DoublyLinkedList()
 {
-    clear();
+clear();
 }
 
-/* ================= Private ================= */
 
 void DoublyLinkedList::clear()
 {
-    Node* current = head;
+Node* current = head;
 
-    while (current)
-    {
-        Node* next = current->next;
-        delete current;
-        current = next;
-    }
-
-    head = nullptr;
-    tail = nullptr;
-    size = 0;
+while (current)
+{
+Node* next = current->next;
+delete current;
+current = next;
 }
 
-/* ================= Public ================= */
-
-void DoublyLinkedList::push_back(int value)
-{
-    Node* newNode = new Node(value);
-
-    if (!head)
-    {
-        head = tail = newNode;
-    }
-    else
-    {
-        tail->next = newNode;
-        newNode->prev = tail;
-        tail = newNode;
-    }
-
-    size++;
+head = nullptr;
+tail = nullptr;
+size = 0;
 }
 
-bool DoublyLinkedList::contains(int value) const
+
+void DoublyLinkedList::push_back(const int value)
 {
-    Node* current = head;
+Node* newNode = new Node(value);
 
-    while (current)
-    {
-        if (current->data == value)
-            return true;
-
-        current = current->next;
-    }
-
-    return false;
+if (!head)
+{
+head = tail = newNode;
+}
+else
+{
+tail->next = newNode;
+newNode->prev = tail;
+tail = newNode;
 }
 
-void DoublyLinkedList::remove(int value)
-{
-    Node* current = head;
-
-    while (current)
-    {
-        if (current->data == value)
-        {
-            if (current->prev)
-                current->prev->next = current->next;
-            else
-                head = current->next;
-
-            if (current->next)
-                current->next->prev = current->prev;
-            else
-                tail = current->prev;
-
-            delete current;
-            size--;
-            return;
-        }
-
-        current = current->next;
-    }
+size++;
 }
 
-void DoublyLinkedList::modify(int oldValue, int newValue)
+bool DoublyLinkedList::contains(const int value) const
 {
-    Node* current = head;
+Node* current = head;
 
-    while (current)
-    {
-        if (current->data == oldValue)
-        {
-            current->data = newValue;
-            return;
-        }
+while (current)
+{
+if (current->data == value)
+return true;
 
-        current = current->next;
-    }
+current = current->next;
+}
+
+return false;
+}
+
+void DoublyLinkedList::remove(const int value)
+{
+Node* current = head;
+
+while (current)
+{
+if (current->data == value)
+{
+if (current->prev)
+current->prev->next = current->next;
+else
+head = current->next;
+
+if (current->next)
+current->next->prev = current->prev;
+else
+tail = current->prev;
+
+delete current;
+size--;
+return;
+}
+
+current = current->next;
+}
+}
+
+void DoublyLinkedList::modify(const int oldValue, const int newValue)
+{
+Node* current = head;
+
+while (current)
+{
+if (current->data == oldValue)
+{
+current->data = newValue;
+return;
+}
+
+current = current->next;
+}
 }
 
 bool DoublyLinkedList::empty() const
 {
-    return size == 0;
+return size == 0;
 }
 
 size_t DoublyLinkedList::getSize() const
 {
-    return size;
+return size;
 }
 
 std::string DoublyLinkedList::toString() const
 {
-    std::ostringstream ss;
-    Node* current = head;
+std::ostringstream ss;
+Node* current = head;
 
-    while (current)
-    {
-        ss << current->data;
+while (current)
+{
+ss << current->data;
 
-        if (current->next)
-            ss << " ";
+if (current->next)
+ss << " ";
 
-        current = current->next;
-    }
+current = current->next;
+}
 
-    return ss.str();
+return ss.str();
 }
 
 std::ostream& operator<<(std::ostream& os, const DoublyLinkedList& list)
 {
-    os << list.toString();
-    return os;
+os << list.toString();
+return os;
 }
